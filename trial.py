eb07b18de75a2b8fa236cb0989c3ee551ee20658
maddoxsabo <maddox.sabo@stu.ocsb.ca>
2024-12-10 18:00:02 +0000

    as

--- trial.py
+++ trial.py
@@ -1,1 +1,28 @@
+print("Welcome to the Pizza Ordering System!
+Pizza Sizes and Prices:")
+
+if x, y, z, = " 1 Small: $8.00" , " 2 Medium: $10.00" , " 3 Large: $12.00"
+print = (x)
+print = (y)
+print = (z)
+
+name = int(input("Can I get a name for your order please: "))
+if name >= "Mr Jamieson"
+    print("Mr Jamieson")
+    print("please choose a pizza (x),(y),(z)") 
+    if x, y, z,
+    print = (x)
+    print = (y)
+    print = (z)
+    
+    print("Order Summary:")
+    if (x) print = (x)
+    print("name")
+    print("Total cost")
+
+
+
+
+else:
+    print(".")
 
